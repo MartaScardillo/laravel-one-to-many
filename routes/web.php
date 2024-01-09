3fb1816ec90ba656f5ea0b79eef9a3e68608d5c1
Marta Scardillo <martascardillo02@gmail.com>
2024-01-09 17:22:35 +0100

    Add projects route

--- routes/web.php
+++ routes/web.php
@@ -27,7 +27,8 @@ Route::middleware('auth')->group(function () {
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
-    Route::resource('admin/projects', ProjectController::class);
+
+    Route::resource('/projects', ProjectController::class)->names('projects.index');
 });
 
 require __DIR__.'/auth.php';
